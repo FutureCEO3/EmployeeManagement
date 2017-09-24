9ba78b962ae06abe1939dd550b1e23095fb5b8bd
FutureCEO3 <30336853+FutureCEO3@users.noreply.github.com>
2017-09-24 12:25:41 +0530

    Added new project  i.e EmployeeManagement

---
+++ EmployeeManagement/Controllers/GajananController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCMembership.Controllers
+{
+    public class GajananController : Controller
+    {
+        [Authorize]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
